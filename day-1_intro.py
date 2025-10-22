e3bf7e67ea6f99dc72319f06e8d0f8fe81ed9ff4
Mohamed Bourhim <164642535+Mokhtarsi@users.noreply.github.com>
2025-10-22 19:32:07 +0100

    Add files via upload

---
+++ day-1_intro.py
@@ -0,0 +1,12 @@
+###-----Python Day 1: Variables, Input, and Output....
+### Date: 2025-10-21
+
+# Ask user for info
+
+Name = input("Enter your name: ")
+Age = input("Enter your age: ")
+Color = input("Enter your favorite color: ")
+
+#Display message 
+print("\nHello", Name + "!")
+print("You are", Age, "years old and your favorite color is", Color +".")
